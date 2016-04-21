394f563860e525dd771a823c42ffc7e145e0eaf8
Jennifer Ford <jenncford@gmail.com>
2016-04-21 14:56:15 -0400

    So have method to sample coulnd't test it specifically but should work

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -7,4 +7,18 @@ class Deck < ActiveRecord::Base
   validates :title, presence: true, uniqueness: {
               scope: :user_id
             }
+
+  def add_cards(cards)
+    deck_cards = cards.map do |name, quantity|
+      card = Card.find_by(name: name)
+      deck_card = DeckCard.new(card_id: card.id, quantity: quantity)
+    end
+    self.deck_cards = self.deck_cards + deck_cards
+  end
+
+  def sample_deck
+    self.sample(7)
+  end
 end
+
+#deck.add_cards([["Forest", 3], ["Plague Rats", 2], ["Bad Moon", 1]])
--- app/models/deck_card.rb
+++ app/models/deck_card.rb
@@ -2,5 +2,5 @@ class DeckCard < ActiveRecord::Base
   belongs_to :deck
   belongs_to :card
 
-  validates_presence_of :deck_id, :card_id, :quantity
+  validates_presence_of :deck_id, :card_id
 end
